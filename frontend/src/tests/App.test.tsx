74789c2ce9129716a200b97da55b51ec4651358e
Eleanor Au <z5308653@ad.unsw.edu.au>
2024-02-16 18:00:30 +1100

    moving tests into their own folder and installing react router

--- frontend/src/tests/App.test.tsx
+++ frontend/src/tests/App.test.tsx
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
-import App from './App';
+import App from '../App';
 
 describe('App', () => {
   it('Renders Charity Management', () => {
